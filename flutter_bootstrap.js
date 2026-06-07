fa8b1ba0c77ad3a48e47b5e88f17b765e8b696d6
xixlai <xixlai@xixlai.xixlai>
2026-06-07 14:14:35 +0200

    fix: base-href correcto para univers-catalog

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3840555324" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3237198095" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
