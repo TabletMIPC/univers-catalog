da3900522db11ed154837eb6f66b08fe933a6241
xixlai <xixlai@xixlai.xixlai>
2026-06-10 21:20:10 +0200

    style: ACTUALIZACION

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4065826383" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1689984398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
